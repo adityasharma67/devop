59ba780e11abe9a26e854f89ec342c95236da24e
adityasharma67 <152427901+adityasharma67@users.noreply.github.com>
2025-10-30 13:16:02 +0530

    Add files via upload

---
+++ math.js
@@ -0,0 +1,7 @@
+function add(a, b) { return a + b; }
+function sub(a, b) { return a - b; }
+function multi(a, b) { return a * b; }
+function div(a, b) { return a / b; }
+const PI = 3.14159;
+
+module.exports = { add, sub, multi, div, PI };
